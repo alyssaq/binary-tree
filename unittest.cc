513c7b6405768f74cab11fe42268fd2cae85951e
alyssaq <alyssaquek@gmail.com>
2013-09-17 02:03:22 +0800

    Removed excess comments

--- unittest.cc
+++ unittest.cc
@@ -1,52 +1,12 @@
-// Copyright 2005, Google Inc.
-// All rights reserved.
+// Unittests using Google C++ testing framework.
 //
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-//     * Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above
-// copyright notice, this list of conditions and the following disclaimer
-// in the documentation and/or other materials provided with the
-// distribution.
-//     * Neither the name of Google Inc. nor the names of its
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// Author: Alyssa Quek
 
-// A sample program demonstrating using Google C++ testing framework.
-//
-// Author: wan@google.com (Zhanyong Wan)
-
-
-// This sample shows how to write a simple unit test for a function,
-// using Google C++ testing framework.
-//
-// Writing a unit test using Google C++ testing framework is easy as 1-2-3:
-
-
-// Step 1. Include necessary header files such that the stuff your
-// test logic needs is declared.
-//
-// Don't forget gtest.h, which declares the testing framework.
 
 #include "tree.h"
-#include "gtest/gtest.h"
+#include "gtest/gtest.h" // Google testing framework.
 
-// Step 2. Use the TEST macro to define your tests.
+// Use the TEST macro to define your tests.
 //
 // TEST has two parameters: the test case name and the test name.
 // After using the macro, you should define your test logic between a
@@ -88,7 +48,7 @@ TEST(TreeTest, add) {
   EXPECT_EQ(3, many.size());
 }
 
-// Step 3. Call RUN_ALL_TESTS() in main().
+// Call RUN_ALL_TESTS() in main().
 //
 // We do this by linking in src/gtest_main.cc file, which consists of
 // a main() function which calls RUN_ALL_TESTS() for us.
@@ -96,6 +56,4 @@ TEST(TreeTest, add) {
 // This runs all the tests you've defined, prints the result, and
 // returns 0 if successful, or 1 otherwise.
 //
-// Did you notice that we didn't register the tests?  The
-// RUN_ALL_TESTS() macro magically knows about all the tests we
-// defined.  Isn't this convenient?
+// RUN_ALL_TESTS() macro magically knows about all the defined tests
